17b32911900462d165db0523149b87fdeddd1935
jelainealexa <jelaine.alexa0903@gmail.com>
2024-02-08 01:52:48 +0800

    Exit and close

--- motion-detector.py
+++ motion-detector.py
@@ -114,9 +114,12 @@ while True:
     
     # Key press to exit loop
     key_pressed = cv2.waitKey(30)
-    
+
     if key_pressed == ord("t"):
         alarm_mode = not alarm_mode
 
     if key_pressed == ord("q"):
         break
+
+cap.release()
+cv2.destroyAllWindows()
