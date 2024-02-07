1046ab037042cf6e010c68b8d71f7c7907d4064c
jelainealexa <jelaine.alexa0903@gmail.com>
2024-02-08 00:49:21 +0800

    Main loop

--- motion-detector.py
+++ motion-detector.py
@@ -17,8 +17,15 @@ start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
 start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
 
 # Main loop
+while True:
+
     # Capture frame from the camera
+    _, frame = cap.read()
+    frame = imutils.resize(frame, width=500)
+
     # Convert frame to grayscale
+    frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     # Apply Gaussian blur to reduce noise
 
     # Compute the difference between the current frame and the start frame
