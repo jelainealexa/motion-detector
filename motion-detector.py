025e1029d5dabb1edacf06dadd5cd88fc915faa9
jelainealexa <jelaine.alexa0903@gmail.com>
2024-02-08 00:46:32 +0800

    Starting frame

--- motion-detector.py
+++ motion-detector.py
@@ -3,13 +3,18 @@
 import cv2
 import imutils
 
+# Set camera properties
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-# Set camera properties
-
 # Read the 1st frame as the starting frame
+
+_, start_frame = cap.read()
+start_frame = imutils.resize(start_frame, width=500)
+start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
+start_frame = cv2.GaussianBlur(start_frame, (21, 21), 0)
 
 # Main loop
     # Capture frame from the camera
